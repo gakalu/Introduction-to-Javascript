1371f757005ad7c16a4e217384a9a50940054fe4
gakalu <69823298+gakalu@users.noreply.github.com>
2021-02-22 19:36:46 -0600

    cs301Assigmentw1d1

---
+++ w1d1Assigment/introDemo.js
@@ -0,0 +1,13 @@
+//Assignment W1D1
+console.log('Hello World');
+console.log('well come to javascript');
+console.log('come to MIU');
+console.log('Hi');
+let x=10;
+let y=20;
+let z='5';
+console.log(x+y);
+console.log(x+y+z);
+console.log(z+y);
+console.log(x+z);
+console.log(x+z+y);
