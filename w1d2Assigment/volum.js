d152ee1715a7a1a95d2d17b7bff7132b4a038bf3
gakalu <69823298+gakalu@users.noreply.github.com>
2021-02-24 11:21:30 -0600

    Update volum.js

--- w1d2Assigment/volum.js
+++ w1d2Assigment/volum.js
@@ -1,9 +1,9 @@
 //w1d2
 let prompt=require('prompt-sync')();
-let value=prompt('Enter the volume with a  quarts   ');//Q1
+let value=prompt('Enter the volume with a  quarts ');//Q1
 let x=parseFloat(value);
 console.log('The volume in litter='+x*0.95)
 console.log('==========================================================');
-let d=prompt('Enter the distance with kilometer   ');//Q2
+let d=prompt('Enter the distance with kilometer  ');//Q2
 let y=parseFloat(d);
-console.log('The distance in miles='+y*0.62);
+console.log('The distance in miles= '+y*0.62);
