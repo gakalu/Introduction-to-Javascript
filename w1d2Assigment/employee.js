4a9a3fac8d45d25a54864662ea4ad8f68912b211
gakalu <69823298+gakalu@users.noreply.github.com>
2021-02-24 11:17:04 -0600

    w1d3

--- w1d2Assigment/employee.js
+++ w1d2Assigment/employee.js
@@ -1,8 +1,8 @@
 //w1d2 Q3
 let prompt=require('prompt-sync')();
-let m =prompt('enter the total number of box:'   );
+let m=prompt('enter the total number of box:'   );
 let totalbox=parseInt(m);
-let n=prompt('enter the number of boxes in each stack:')
+let n=prompt('enter the number of boxes in each stack: ')
 let boxperstack=parseInt(n);
 let result=totalbox%boxperstack;
 if(result==0){
