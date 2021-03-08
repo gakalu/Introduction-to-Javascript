e1cf506848c92419513336ac4476934c069b68ff
gakalu <69823298+gakalu@users.noreply.github.com>
2021-03-08 17:40:25 -0600

    onlyNameDifferance.

--- Assignmentw2d5/Assignmentw2d5.js
+++ Assignmentw2d5/Assignmentw2d5.js
@@ -1,9 +1,11 @@
+"use strict";
+
 exports.isArrayEqual = isArrayEqual;
-exports.checkspalindrome= checkspalindrome;
-exports.transform= transform;
+exports.isPalindrome= isPalindrome;
+exports.reverse2String= reverse2String;
 exports.enhancedIncludes=enhancedIncludes;
-exports.reversedcopy = reversedcopy;
-exports.mergeArray = mergeArray;
+exports.ssReverse = ssReverse;
+exports.merge = merge;
 
 //W2D5 Q1
 function isArrayEqual(arr1,arr2){
@@ -19,7 +21,7 @@ console.log('Q1 =============check equality of an array=========');
 console.log(isArrayEqual([2,3,4],[2,3,8]));
 
 //w2d5 Q2
-function checkspalindrome(arr3){
+function isPalindrome(arr3){
     let revarr=[];
     for(let i=0;i<arr3.length;i++){
     revarr.push(arr3[(arr3.length-1)-i]);
@@ -31,17 +33,17 @@ function checkspalindrome(arr3){
 }
 let arr3=[5,3,5,3]
 console.log('Q2 =============check for palandrom=========');
-console.log(checkspalindrome(arr3));
+console.log(isPalindrome(arr3));
 
 //w2d5 Q3
-function transform(arr2){
+function reverse2String(arr2){
 
     let text=arr2.reverse();
     let final=text.join("_")
     return final;
 }
 console.log('Q3 =============teansform to given format======');
-console.log(transform(['Hi','Myname','is','Akalu']));
+console.log(reverse2String(['Hi','Myname','is','Akalu']));
 
 //w2d5 Q4
 function enhancedIncludes(arr1,value){
@@ -71,7 +73,7 @@ console.log('Q4 =============check for inclusive=========');
 console.log(enhancedIncludes(v1, v2));
 
 //w2d5 Q5
-function reversedcopy(arr){
+function ssReverse(arr){
     let newArr=[];
     for(let i=0;i<arr.length;i++){
     let element=+arr.slice(arr.length-(i+1),arr.length-i);
@@ -81,12 +83,12 @@ function reversedcopy(arr){
 }
 let arr=[1,2,3,4];
 console.log('Q5 =============The reversed array================');
-console.log(reversedcopy(arr));
+console.log(ssReverse(arr));
 console.log('Q5=============The orginal array=================');
 console.log(arr);
 
 //w2d5 Q6
-function mergeArray(arr1, arr2) {
+function merge(arr1, arr2) {
     let n=arr1.length+arr2.length;
     for (let i=arr1.length; i<n;i++) {
     arr1.push(arr2.shift());
@@ -106,7 +108,7 @@ function mergeArray(arr1, arr2) {
     }
     return arr;
    }
-   let arr4= [1,9,5];
-   let arr2 = [6, 8];
+   let arr4= [1,3,8];
+   let arr2 = [0,5,9];
    console.log('Q6 =============marging of an array=========');
-   console.log(mergeArray(arr4, arr2));
+   console.log(merge(arr4, arr2));
--- Assignmentw2d5/mocaw2d5test.js
+++ Assignmentw2d5/mocaw2d5test.js
@@ -1,59 +1,101 @@
 "use strict";
-const assert = require("assert");
-const arrays = require("../Asignw2d5");
-const isArrayEqual= require("../Asignw2d5").isArrayEqual;
 
-/* Write a function, isArrayEqual, that returns true if two arrays have === elements, else false; */
+const assert = require("assert");
+//const arrays = require("./arrayMethods.js");
+const arrays = require("./Assignmentw2d5");
+
+
+/* 1.	Write a function, isArrayEqual, that returns true if two arrays have === elements, else false; */
 describe("isArrayEqual", function () {
 
-    it("Either true or false", function () {
-        assert.deepStrictEqual(arrays.isArrayEqual(([1, 2, 5],[1,2,5]),ture));
+    it("1 2 3 true", function () {
+        assert.strictEqual(arrays.isArrayEqual([1, 2, 3], [1, 2, 3]), true);
     });
- 
-});
-/* Write a function that checks if an array is palindrome by using push and pop array methods to 
-reverse the array and then isArrayEqual method you wrote for 1*/ 
-describe("checkspalindrome", function () {
+    it(" 1 2 3 4 5 false", function () {
+        assert.strictEqual(arrays.isArrayEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6]), false);
+    });
 
-    it("check for palandrom", function () {
-        assert.deepStrictEqual(arrays.checkspalindrome(([5, 3, 5,3,5]),true));
-    });
- 
 });
-/* Write a function that transforms a given array as following. Use appropriate array methods.
-Input (Array)  ['Quick', 'Brown', 'Fox']
-Output (String) "Fox_Brown_Quick" */
-describe("transform", function () {
 
-    it("Check for changed format", function () {
-        assert.deepStrictEqual(arrays.transform(['Quick', 'Brown', 'Fox'],Fox_Brown_Quick));
-    });
- 
-});
-/* Write a JavaScript function named enhancedIncludes that takes two parameters, an array, and a 
-value to search in the array and return an array result with three values.*/
-describe("enhancedInclude", function () {
 
-    it("Check for inclustion of a value", function () {
-        assert.deepStrictEqual(arrays.enhancedInclude(([1, 2, 5],5)),[true,-1,5]);
+/* 2.	Write a function that checks if an array is palindrome by using push and pop array methods to reverse the 
+array and then isArrayEqual method you wrote for 1.  */
+describe("palindrome via push and pop", function () {
+
+    it("aba true", function () {
+        assert.strictEqual(arrays.isPalindrome(["a", "b", "a"]), true);
     });
- 
+    it("abc false", function () {
+        assert.strictEqual(arrays.isPalindrome(["a", "b", "c"]), false);
+    });
+    it("rotator true", function () {
+        assert.strictEqual(arrays.isPalindrome(["r", "o", "t", "a", "t", "o", "r"]), true);
+    });
 });
-/* Write a function that returns reversed copy of a given array (original array should 
-remain intact). Do this without using reverse method, instead use splice and slice 
-methods.
+
+/* 3.      Write a function, reverse2String,  that transforms a given array as following. Use appropriate array methods. 
+Input (Array) 	Output (String) 
+['Quick', 'Brown', 'Fox'] 	"Fox_Brown_Quick" */
+describe("reverse2String", function () {
+
+    it("Fox_Brown_Quick", function () {
+        assert.strictEqual(arrays.reverse2String(["Quick", "Brown", "Fox"] ), "Fox_Brown_Quick");
+    });
+
+    it("4_3_2_1", function () {
+        assert.strictEqual(arrays.reverse2String([1, 2, 3, 4] ), "4_3_2_1");
+    });
+});
+
+
+
+/*
+    4.      Write a JavaScript function named enhancedIncludes that takes two parameters, an array, and a value to search in the array and return an array result with three values.
+    a.	First value is boolean representing if the search value exists in the array.
+    b.	Second value is the first index of value found in the array or -1
+    c.	Third value is the last index of value found in the array or -1.
 */
-describe("reversedcopy", function () {
+describe("enhancedIncludes", function () {
 
-    it("Reverse an array using slice", function () {
-        assert.deepStrictEqual(arrays.reversedcopy([1, 2, 5],[5,2,1]));
+    it("[1, 2, 3, 4, 2], 2 ]", function () {
+        assert.deepStrictEqual(arrays.enhancedIncludes([1, 2, 3, 4, 2], 2 ), [true, -1, 2]);
     });
- 
-});
-describe("mergeArray", function () {
 
-    it("Marge two array and sort", function () {
-        assert.deepStrictEqual(arrays.mergeArray([1, 2, 5,8,3,10],[9,8,6,7]),[1,2,3,5,6,7,8,8,9,]);
-    });
- 
 });
+
+/* 5. Write a function, ssReverse, that returns reversed copy of a given array (original array should remain intact). 
+Do this without using reverse method, instead use splice and slice methods.  */
+describe("ssReverse", function () {
+
+    it("[1, 2, 3, 4]", function () {
+        assert.deepStrictEqual(arrays.ssReverse([1, 2, 3, 4 ]), [4,3,2,1] );
+    });
+});
+
+// hints for 5:
+// - which is appropriate for making a copy of an array, splice or slice?
+// - can you write a function that takes an index and an array and a newElement and replaces the element at the index with the newElement?
+//   e.g., frontReplace(2,[1, 2,3,4,5,6,7],99) --> [1,2,99,4,5,6,7)
+// - can you write a function that takes an index and an array and a newElement and replaces the element at the length - 1 - index with the newElement?
+//  e.g., backReplace(2,[1, 2,3,4,5,6,7],99) --> [1,2,3,4,99,6,7)
+// - can you write a function that takes an index and an array and returns a string with the elements at positions index and length - 1 - index
+//   e.g., getIndices(2,[1, 2,3,4,5,6,7]) --> "35"
+
+/*   6. (EC) Write a function that merges two sorted arrays into one single sorted array. Make use of shift and push array methods. */
+describe("merge via shift and push", function () {
+    it("138 merge with 059 ", function () {
+        console.log(arrays.merge([1, 3, 8], [0, 5, 9]));
+        assert.deepStrictEqual(arrays.merge([1, 3, 8], [0, 5, 9]), [0, 1, 3, 5, 8, 9]);
+    });
+});
+
+/*  hints for 6
+There are various solutions to the problem of merging two sorted arrays.  The following, shiftPush, might be helpful
+as a helper method in an approach that goes through the elements of the first array and merges each with the second array.
+function shiftPush(shiftArray, target, addArray) 
+shiftArray is a sorted array, take the elements in it that are less than target, and push them onto addArray 
+*/
+    // it("shiftPush helper", function () {
+    //     console.log(arrays.shiftPush([1, 3, 8], 4, []));
+    //     assert.deepStrictEqual(arrays.shiftPush([1, 3, 8], 4, []), [1, 3]);
+    // });
