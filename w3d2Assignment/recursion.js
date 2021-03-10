392450a0c1b531a8430fd49043177e92324e84e5
gakalu <69823298+gakalu@users.noreply.github.com>
2021-03-10 10:18:16 -0600

    quz

--- w3d2Assignment/recursion.js
+++ w3d2Assignment/recursion.js
@@ -1,8 +1,8 @@
 "use strict";
 
 exports.power = power;
-exports.countDigits= countDigits;
-exports.reverse=reverse;
+// exports.countDigits= countDigits;
+// exports.reverse=reverse;
 //w3d2 Q1
 function power(a,b){
     if(b===0){
@@ -17,20 +17,20 @@ function power(a,b){
 console.log(power(4,-2));//expected .0.0625
 console.log(power(4,2))//expected 16
 //w3d2 Q2
-function countDigits(number){
-    if(number===0){
-        return 0;
-    }else {
-        return 1+countDigits(Math.floor(number/10));  
-    }
-}
-console.log(countDigits(456));//expected 3
-//w3d2 Q3
-function reverse(str){
-    if(str===""){
-    return '';
-    }else{
-    return reverse(str.substring(1))+str.substring(0,1);
-    }
-}
-console.log(reverse(" Getachew"));//wehcateG
+// function countDigits(number){
+//     if(number===0){
+//         return 0;
+//     }else {
+//         return 1+countDigits(Math.floor(number/10));  
+//     }
+// }
+// console.log(countDigits(456));//expected 3
+// //w3d2 Q3
+// function reverse(str){
+//     if(str===""){
+//     return '';
+//     }else{
+//     return reverse(str.substring(1))+str.substring(0,1);
+//     }
+// }
+// console.log(reverse(" Getachew"));//wehcateG
